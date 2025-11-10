7525642bd6a631043a7ed462c8f5e92b51d5bb18
Nihal Singh <furiousyeti0715@gmail.com>
2025-11-11 03:34:19 +0530

    Update TT_gen.py

--- TT_gen.py
+++ TT_gen.py
@@ -19,6 +19,8 @@ import json
 # ---------------------------
 # Constants and durations (minutes)
 # ---------------------------
+
+os.makedirs("outputs", exist_ok=True)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.join(BASE_DIR, "config.json")
 try:
